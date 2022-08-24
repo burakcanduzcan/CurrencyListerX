dfb4109a83ae0a35cc40f98b19727988f61205b9
Burakcan Duzcan <burakcanduzcan@gmail.com>
2022-08-24 22:39:37 +0300

    added updates section with 3 rss feed source

--- app/src/main/java/com/burakcanduzcan/currencylisterx/ui/updates/UpdatesFragment.kt
+++ app/src/main/java/com/burakcanduzcan/currencylisterx/ui/updates/UpdatesFragment.kt
@@ -1,15 +1,26 @@
 package com.burakcanduzcan.currencylisterx.ui.updates
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.burakcanduzcan.currencylisterx.databinding.FragmentUpdatesBinding
+import com.google.android.material.tabs.TabLayout
+import com.prof.rssparser.Article
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+@AndroidEntryPoint
 class UpdatesFragment : Fragment() {
 
     private lateinit var binding: FragmentUpdatesBinding
+    private val viewModel: UpdatesViewModel by viewModels()
+    private lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,7 +28,61 @@ class UpdatesFragment : Fragment() {
     ): View {
         binding = FragmentUpdatesBinding.inflate(inflater)
 
+        Timber.i("UpdatesFragment onCreateView")
+        initializeViews()
+
         return binding.root
     }
 
+    override fun onResume() {
+        super.onResume()
+        Timber.i("UpdatesFragment onResume")
+    }
+
+    private fun initializeViews() {
+        //tabLayout
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (binding.tabLayout.selectedTabPosition) {
+                    0 -> {
+                        Timber.i("News tab selected: Cointelegraph")
+                        viewModel.getLatestNews("Cointelegraph")
+                    }
+                    1 -> {
+                        Timber.i("News tab selected: CoinDesk")
+                        viewModel.getLatestNews("CoinDesk")
+                    }
+                    2 -> {
+                        Timber.i("News tab selected: CoinJournal")
+                        viewModel.getLatestNews("CoinJournal")
+                    }
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+        })
+        //recyclerView
+        binding.rvNews.apply {
+            newsAdapter = NewsAdapter(::onNewsClick)
+            this.adapter = newsAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+        //observe
+        viewModel.articleList.observe(viewLifecycleOwner) {
+            newsAdapter.submitList(it)
+            //updateProgressBar()
+        }
+    }
+
+    private fun onNewsClick(article: Article) {
+        val url = article.link
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        startActivity(intent)
+    }
 }
--- app/src/main/java/com/burakcanduzcan/currencylisterx/ui/updates/UpdatesViewModel.kt
+++ app/src/main/java/com/burakcanduzcan/currencylisterx/ui/updates/UpdatesViewModel.kt
@@ -1,6 +1,47 @@
 package com.burakcanduzcan.currencylisterx.ui.updates
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.prof.rssparser.Article
+import com.prof.rssparser.Channel
+import com.prof.rssparser.Parser
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class UpdatesViewModel : ViewModel() {
+@HiltViewModel
+class UpdatesViewModel @Inject constructor(
+    private val parser: Parser,
+) : ViewModel() {
+    private val _articleList = MutableLiveData<List<Article>>()
+    val articleList: LiveData<List<Article>> = _articleList
+
+    init {
+        getLatestNews("Cointelegraph")
+    }
+
+    fun getLatestNews(source: String) {
+        viewModelScope.launch(Dispatchers.IO) {
+            when (source) {
+                "Cointelegraph" -> {
+                    val channel: Channel =
+                        parser.getChannel(url = "https://cointelegraph.com/rss")
+                    _articleList.postValue(channel.articles)
+                }
+                "CoinDesk" -> {
+                    val channel: Channel =
+                        parser.getChannel(url = "https://www.coindesk.com/arc/outboundfeeds/rss/")
+                    _articleList.postValue(channel.articles)
+                }
+                "CoinJournal" -> {
+                    val channel: Channel =
+                        parser.getChannel(url = "https://coinjournal.net/news/category/analysis/feed/")
+                    _articleList.postValue(channel.articles)
+                }
+            }
+        }
+    }
 }
